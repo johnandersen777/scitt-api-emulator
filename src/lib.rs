1662b3e66b64b73ac44f7e4b6970ea3ff20c69d6
John Andersen <johnandersenpdx@gmail.com>
2024-07-11 19:56:01 -0700

    feat: successfully calling rust from python
    Asciinema: https://asciinema.org/a/667710
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -157,6 +157,8 @@ enum ValidationError {
     CustomError(StringError),
 }
 
+// pyo3::exceptions::impl_native_exception!(ValidationError, pyo3::exceptions::PyExc_Exception, pyo3:exceptions::native_doc!("ValidationError"));
+
 impl fmt::Display for ValidationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -221,7 +223,9 @@ impl PolicyEngineWorkflow {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+// fn main() -> Result<(), Box<dyn Error>> {
+/// Formats the sum of two numbers as string.
+fn parse_policy_engine_request() -> Result<(), Box<dyn Error>> {
     let json_data = r#"
         {
             "context": {
@@ -283,17 +287,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-/// Formats the sum of two numbers as string.
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn py_parse_policy_engine_request() -> PyResult<()> {
+    parse_policy_engine_request();
+
+    Ok(())
 }
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
-fn string_sum(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn scitt_api_emulator_rust_policy_engine(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_parse_policy_engine_request, m)?)?;
     Ok(())
 }
