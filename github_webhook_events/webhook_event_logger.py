9b2e606355d27ecb4ecc2274c114ac6ce73c5c93
John <johnandersenpdx@gmail.com>
2024-10-23 06:38:41 -0700

    indent data
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- github_webhook_events/webhook_event_logger.py
+++ github_webhook_events/webhook_event_logger.py
@@ -34,7 +34,7 @@ class GitHubWebhookLogger(http.server.BaseHTTPRequestHandler):
             )
         )
 
-        payload_path.write_text(json.dumps(payload))
+        payload_path.write_text(json.dumps(payload, indent=4, sort_keys=True))
 
     @classmethod
     def cli(cls, argv=None):
