c06dc2d4e8a85fcbb9e607a17067daf51800bb02
John <johnandersenpdx@gmail.com>
2024-10-23 06:38:31 -0700

    start.sh
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- Procfile
+++ Procfile
@@ -1,7 +1,1 @@
-web: |
-  scitt-emulator server \
-    --tree-alg CCF \
-    --port "${PORT}" \
-    --workspace ${HOME}/Documents/fediverse/scitt_federation_alice/workspace_alice/ \
-    --middleware scitt_emulator.federation_activitypub_bovine:SCITTFederationActivityPubBovine \
-    --middleware-config-path ${HOME}/Documents/fediverse/scitt_federation_alice/config.json
+web: bash -xeou start.sh
---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+rm -rf workspace/ federation_workspace/
+
+mkdir -pv workspace/storage/operations/ federation_workspace/
+
+tee federation_workspace/config.json <<EOF
+{
+  "handle_name": "demo",
+  "fqdn": "scitt.unstable.chadig.com",
+  "workspace": "${PWD}/federation_workspace/",
+  "bovine_db_url": "sqlite://${PWD}/federation_workspace/bovine.sqlite3",
+  "following": {}
+}
+EOF
+
+scitt-emulator server \
+  --tree-alg CCF \
+  --port "${PORT}" \
+  --workspace workspace/ \
+  --middleware scitt_emulator.federation_activitypub_bovine:SCITTFederationActivityPubBovine \
+  --middleware-config-path federation_workspace/config.json
