86fd44983517ab98c09c759a6bb2f44d34b11aff
John <johnandersenpdx@gmail.com>
2024-10-23 06:38:32 -0700

    Add SphinxDocsMiddleware
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ sphinx_docs_middleware.py
@@ -0,0 +1,54 @@
+# Copyright (c) SCITT Authors.
+# Licensed under the MIT License.
+import sys
+import pathlib
+import subprocess
+
+from quart import send_from_directory
+
+from scitt_emulator.signals import SCITTSignals
+
+
+class SphinxDocsMiddleware:
+    def __init__(self, app, signals: SCITTSignals, config_path: pathlib.Path):
+        self.app = app
+        self.asgi_app = app.asgi_app
+        self.signals = signals
+        self.config = {}
+        if config_path and config_path.exists():
+            self.config = json.loads(config_path.read_text())
+
+        self.build_docs()
+        self.add_routes(app)
+
+    def build_docs(self):
+        parent_path = pathlib.Path(__file__).parent
+        self.built_singlehtml_dir_path = parent_path.joinpath("built_singlehtml_dir")
+        subprocess.call(
+            [
+                sys.executable,
+                "-m",
+                "sphinx",
+                "-b",
+                "singlehtml",
+                parent_path.joinpath("docs"),
+                self.built_singlehtml_dir_path,
+            ]
+        )
+
+    def add_routes(self, app):
+        # TODO Blueprint?
+        @app.get("/")
+        async def index():
+            return await send_from_directory(
+                self.built_singlehtml_dir_path, "index.html"
+            )
+
+        @app.get("/_static/<path:path>")
+        async def static_file(path):
+            return await send_from_directory(
+                self.built_singlehtml_dir_path.joinpath("_static"), path
+            )
+
+    async def __call__(self, scope, receive, send):
+        return await self.asgi_app(scope, receive, send)
