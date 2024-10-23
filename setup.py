44f71f4fbc234a74d35828b7e76b3839a2b40ffb
John <johnandersenpdx@gmail.com>
2024-10-23 06:38:37 -0700

    Add setup.py
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Copyright (c) SCITT Authors.
+# Licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="scitt-software-supply-chain-middleware",
+    version="0.0.1",
+    packages=find_packages(),
+    python_requires=">=3.11",
+)
